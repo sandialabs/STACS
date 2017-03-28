cd7a81aeec0e1f0fce533a337874df09b089375a
Felix Wang <felixwang01@gmail.com>
2017-03-28 02:19:52 -0500

    Add second half of trace to STDP model

--- models/izhi_syn_stdp.C
+++ models/izhi_syn_stdp.C
@@ -19,14 +19,16 @@ class IzhiSynSTDP : public NetModelTmpl < 12, IzhiSynSTDP > {
       paramlist[1] = "tau";
       paramlist[2] = "update";
       // states
-      statelist.resize(3);
+      statelist.resize(4);
       statelist[0] = "weight";
       statelist[1] = "wdelta";
-      statelist[2] = "trace";
+      statelist[2] = "ptrace";
+      statelist[3] = "ntrace";
       // sticks
-      sticklist.resize(2);
+      sticklist.resize(3);
       sticklist[0] = "delay";
-      sticklist[1] = "tlast";
+      sticklist[1] = "ptlast";
+      sticklist[2] = "ntlast";
       // auxiliary states
       auxstate.resize(1);
       auxstate[0] = "I";
@@ -78,9 +80,11 @@ void IzhiSynSTDP::Jump(const event_t& evt, std::vector<std::vector<real_t>>& sta
       // Apply effect to neuron (vertex)
       state[0][aux[0].stateidx[0]] += state[e][0];
       // Compute trace value
-      state[e][2] = state[e][2]*exp(-((real_t) (evt.diffuse - stick[e][1])/TICKS_PER_MS)/param[1]);
+      state[e][3] = state[e][3]*exp(-((real_t) (evt.diffuse - stick[e][2])/TICKS_PER_MS)/param[1]);
       // Depress weight
-      state[e][1] -= 1.2 * state[e][2];
+      state[e][1] += state[e][3];
+      // Reset trace to 0.1 when pre-synaptic neuron fires
+      state[e][2] = 0.1;
       // Update last event value for calculating trace
       stick[e][1] = evt.diffuse;
     }
@@ -91,10 +95,10 @@ void IzhiSynSTDP::Jump(const event_t& evt, std::vector<std::vector<real_t>>& sta
       state[e][2] = state[e][2]*exp(-((real_t) (evt.diffuse - stick[e][1])/TICKS_PER_MS)/param[1]);
       // Facilitate weight
       state[e][1] += state[e][2];
-      // Reset trace to 0.1 when post-synaptic neuron fires
-      state[e][2] = 0.1;
+      // Reset negative trace to -0.12 when post-synaptic neuron fires
+      state[e][3] = -0.12;
       // Update last event value for calculating trace
-      stick[e][1] = evt.diffuse;
+      stick[e][2] = evt.diffuse;
     }
   }
   else if (evt.type == EVENT_EDGUP) {
