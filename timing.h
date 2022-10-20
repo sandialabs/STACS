9f0226c4392a307844b11843854987629bfe9440
Felix Wang <felwang@sandia.gov>
2022-10-20 13:20:02 -0600

    updating default timings

--- timing.h
+++ timing.h
@@ -13,9 +13,9 @@
 #define TSTEP_DEFAULT    1.0 // 1ms
 #define TEVENTQ_DEFAULT  20.0 // 20ms
 #define TDISPLAY_DEFAULT 100.0 // 100ms
-#define TRECORD_DEFAULT  500.0 // 500ms
-#define TBALANCE_DEFAULT 5000.0 // 5s
+#define TRECORD_DEFAULT  1000.0 // 1s
 #define TSAVE_DEFAULT    2000.0 // 2s
+#define TBALANCE_DEFAULT 5000.0 // 5s
 #define TMAX_DEFAULT     10000.0 // 10s
 #define EPISODES_DEFAULT 20 // 20 episodes
 #define TEPISODE_DEFAULT 500.0 // 500ms
