a005774a7afc7e217a9f3e5743e1c31f859973ad
felwang-sandia <55103173+felwang-sandia@users.noreply.github.com>
2026-07-20 16:44:03 -0600

    More flexible synapses

--- models/switch_synapse.C
+++ models/switch_synapse.C
@@ -9,15 +9,16 @@
 /**************************************************************************
 * Class declaration
 **************************************************************************/
-class SimpleSynapse : public ModelTmpl < ModelHash("simple_synapse"), SimpleSynapse > {
+class SwitchSynapse : public ModelTmpl < ModelHash("switch_synapse"), SwitchSynapse > {
   public:
     /* Constructor */
-    SimpleSynapse() {
+    SwitchSynapse() {
       // parameters
       paramlist.resize(0);
       // states
-      statelist.resize(1);
+      statelist.resize(2);
       statelist[0] = "weight";
+      statelist[1] = "switch";
       // sticks
       sticklist.resize(1);
       sticklist[0] = "delay";
@@ -41,24 +42,26 @@ class SimpleSynapse : public ModelTmpl < ModelHash("simple_synapse"), SimpleSyna
 
 // Simulation step
 //
-tick_t SimpleSynapse::Step(tick_t tdrift, tick_t tdiff, std::vector<real_t>& state, std::vector<tick_t>& stick, std::vector<event_t>& events) {
+tick_t SwitchSynapse::Step(tick_t tdrift, tick_t tdiff, std::vector<real_t>& state, std::vector<tick_t>& stick, std::vector<event_t>& events) {
   return tdiff;
 }
 
 // Simulation jump
 //
-void SimpleSynapse::Jump(const event_t& event, std::vector<std::vector<real_t>>& state, std::vector<std::vector<tick_t>>& stick, const std::vector<auxidx_t>& auxidx) {
+void SwitchSynapse::Jump(const event_t& event, std::vector<std::vector<real_t>>& state, std::vector<std::vector<tick_t>>& stick, const std::vector<auxidx_t>& auxidx) {
   // External spike event
+  // Affected state is flexibly indexed by the switch (e.g. multiple input accumulators)
+  // Note: be careful switch value doesn't exceed input accumulators
   if (event.type == EVENT_SPIKE && event.source >= 0) {
     // Apply effect to neuron (vertex)
-    state[0][auxidx[0].stateidx[0]] += state[event.index][0];
+    state[0][auxidx[0].stateidx[0] + ((std::size_t) state[event.index][1])] += state[event.index][0];
   }
   else if (event.type == EVENT_CURRENT && event.source >= 0) {
     // Current (graded spike) multiply by weight
-    state[0][auxidx[0].stateidx[0]] += state[event.index][0] * event.data;
+    state[0][auxidx[0].stateidx[0] + ((std::size_t) state[event.index][1])] += state[event.index][0] * event.data;
   }
   else if (event.type == EVENT_STIM && event.source >= 0) {
     // Direct stimulation (just add event payload)
-    state[0][auxidx[0].stateidx[0]] += event.data;
+    state[0][auxidx[0].stateidx[0] + ((std::size_t) state[event.index][1])] += event.data;
   }
 }
